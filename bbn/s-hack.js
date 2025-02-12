e1981e661ddfdf4a3bd7f23e3f1e8abeaa714a7d
softdoritos <connor@fogwave.com>
2025-02-11 19:19:47 -0800

    Create s-hack.js
    test commit of a js script from bbn game

---
+++ bbn/s-hack.js
@@ -0,0 +1,16 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const target = ns.args[0]
+    const moneyThresh = ns.getServerMaxMoney(target) * .95;
+    const securityThresh = ns.getServerMinSecurityLevel(target) * 1.05;
+
+    while(true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
+}
