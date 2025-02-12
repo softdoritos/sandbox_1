bc6723b3d73a630c15b3083942e70a0685086f50
softdoritos <connor@fogwave.com>
2025-02-11 19:29:12 -0800

    Update s-hack.js
    Updated the comment with usage.

--- bbn/s-hack.js
+++ bbn/s-hack.js
@@ -1,4 +1,7 @@
-/** @param {NS} ns */
+/**
+ * Usage in game: 'run s-hack.js <server>'
+ * @param {NS} ns
+ */
 export async function main(ns) {
     const target = ns.args[0]
     const moneyThresh = ns.getServerMaxMoney(target) * .95;
